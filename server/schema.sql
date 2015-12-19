54970e64da53087d15c8f3dce0e67416787b40fc
Santosh Gautam <santoshgautam7@gmail.com>
2015-12-18 17:17:09 -0800

    Connection to database.

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,12 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageId int NOT NULL AUTO_INCREMENT,
+  userId int,
+  text varchar(150),
+  timestamp timestamp,
+  roomname varchar(40),
+  PRIMARY KEY (messageId)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -12,6 +18,7 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
+      //must run from directory and not in SQL
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
